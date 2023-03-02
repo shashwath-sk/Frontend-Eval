8bdbd6c5248f367bce626fb8537d9cd6b36c87e3
shashwath sk <shashukarkera01@gmail.com>
2023-03-02 20:46:21 +0530

    feat: wrap all components which will be using theme context by context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
 // eslint-disable-next-line import/no-unresolved
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
@@ -7,22 +8,27 @@ import {
 } from './pages';
 import { HOME_ROUTE, ERROR_ROUTE, EVENT_ROUTE } from './constants/routes';
 import { Header, Footer } from './components';
+import ThemeContext from './context/PageThemeContext';
 
 function App() {
+  const [theme, setTheme] = React.useState('theme-black');
+  const value = { theme, setTheme };
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path={HOME_ROUTE} element={<HomePage />} />
-          <Route path={EVENT_ROUTE} element={<EventPage />} />
-          <Route path={ERROR_ROUTE} element={<ErroPage />} />
+        <ThemeContext.Provider value={value}>
+          <Header />
+          <Routes>
+            <Route path={HOME_ROUTE} element={<HomePage />} />
+            <Route path={EVENT_ROUTE} element={<EventPage />} />
+            <Route path={ERROR_ROUTE} element={<ErroPage />} />
 
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-        {/* <Footer/> */}
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+          <Footer />
+        </ThemeContext.Provider>
       </BrowserRouter>
-      <Footer />
     </div>
   );
 }
